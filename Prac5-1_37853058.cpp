4edca79e43a3419c60c144a352c59bf7b0a013a0
Thuso Ndou <104587370+Thuso-Ndou@users.noreply.github.com>
2023-08-24 11:24:16 +0200

    Add files via upload

---
+++ Prac5-1_37853058.cpp
@@ -0,0 +1,135 @@
+/*
+Name: Thuso
+Surname: Ndou
+Student Number: 37853058
+
+A C++ program that will store details about different movies in an array of structures
+and allow users to sort and display them based on different criteria.
+*/
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <algorithm>
+
+// Using the standard namespace to avoid prefixing with "std::"
+using namespace std;
+
+//Add struct declaration here
+struct Movie {
+    string title;
+    int year;
+    float rating;
+    string genre;
+};
+
+
+// Function prototypes
+void inputMovies(Movie arrMovies[], int n);
+void displayMovies(Movie arrMovies[], int n);
+
+bool sortByTitle(Movie a, Movie b);
+bool sortByRating(Movie a, Movie b);
+
+int main()
+{
+    int n;
+    // Prompt the user for the number of movies they want to input
+    cout << "How many movies do you want to input? ";
+    cin >> n;
+
+    // ADD CODE HERE <Declare an array of Movie structs to store the movie details called arrMovies> ADD CODE HERE
+
+     Movie arrMovies[n];
+
+
+    //============================================================================================================
+
+    // Call the inputMovies function to get movie details from the user
+    inputMovies(arrMovies, n);
+
+    int choice;
+
+    do
+    {
+        // Display the sorting options menu to the user
+        cout << "\nChoose a sorting option:\n";
+        cout << "1. Sort by title\n";
+        cout << "2. Sort by IMDb rating\n";
+        cout << "3. Exit\n";
+        cout << "\nYour choice: ";
+        cin >> choice;
+
+        // Use a switch-case to handle the user's choice
+        switch (choice)
+        {
+            case 1:
+                // Sort the movies array by title
+                sort(arrMovies, arrMovies + n, sortByTitle);
+                break;
+            case 2:
+                // Sort the movies array by IMDb rating
+                sort(arrMovies, arrMovies + n, sortByRating);
+                break;
+            case 3:
+                // Exit the program
+                cout << "Exiting...\n";
+                return 0;
+            default:
+                // Handle invalid choices
+                cout << "Invalid choice. Try again.\n";
+                continue;
+        }
+
+        // Display the sorted list of movies
+        displayMovies(arrMovies, n);
+    }
+    while (choice != 5);
+
+    return 0;
+}
+// ADD CODE BELOW
+// Function to get movie details from the user
+void inputMovies(Movie arrMovies[], int n) {
+    cin.ignore(); // Clear the newline character from the previous input
+    for (int i = 0; i < n; ++i) {
+        cout << "\nEnter details for movie " << i + 1 << ":\n";
+        cout << "Title: ";
+        getline(cin, arrMovies[i].title);
+        cout << "Year: ";
+        cin >> arrMovies[i].year;
+        cout << "Rating: ";
+        cin >> arrMovies[i].rating;
+        cin.ignore(); // Clear the newline character from the previous input
+        cout << "Genre: ";
+        getline(cin, arrMovies[i].genre);
+    }
+}
+
+// Function to display the list of movies
+void displayMovies(Movie arrMovies[], int n) {
+    // heading
+    cout << "\nMovies:\n";
+
+    for (int i = 0; i < n; ++i) {
+        // display movie title
+        cout << left << setw(20) << arrMovies[i].title << " | ";
+        // display the year
+        cout << left << setw(5) << arrMovies[i].year << " | ";
+        //display movie rating
+        cout << left <<  setw(6) << fixed << setprecision(1) << arrMovies[i].rating << " | ";
+        // display movie genre
+        cout << arrMovies[i].genre << "\n";
+    }
+}
+
+// Comparison function to sort movies by title
+bool sortByTitle(Movie a, Movie b) {
+    return a.title < b.title;
+}
+
+// Comparison function to sort movies by IMDb rating in descending order
+bool sortByRating(Movie a, Movie b) {
+    return a.rating > b.rating;
+}
+
+
---
+++ Prac5-2_37853058.cpp
@@ -0,0 +1,144 @@
+/*
+Name: Thuso
+Surname: Ndou
+Student Number: 37853058
+
+A C++ program to track and manage user concerns about data privacy on various
+online platforms. The program will use parallel arrays: one to store the name
+of the platform, and a parallel array to store a numerical value representing
+the level of user concern.
+*/
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Constants for the maximum number of platforms
+const int MAX_PLATFORMS = 100;
+
+// Function to display all platforms and their associated concern levels in column format
+void displayPlatforms(string arrPlatformNames[], int arrConcernLevels[], int currentSize)
+{
+    cout << "No.   Platform           Concern Level\n";
+
+    for (int i = 0; i < currentSize; ++i)
+    {
+        cout << i + 1 << ".    " << arrPlatformNames[i] << "               " << arrConcernLevels[i] << "\n";
+    }
+}
+
+// Function to add a new platform and its associated concern level
+void addPlatform(string arrPlatformNames[], int arrConcernLevels[], int &currentSize); // what does the & mean before a parameter name???
+
+// Function to update the concern level for an existing platform
+void updateConcernLevel(string arrPlatformNames[], int arrConcernLevels[], int currentSize);
+
+
+int main()
+{
+    int choice;
+    int currentSize = 4;  // Start with 4 initial records
+
+
+    // ADD CODE HERE <Declare both arrays, namely arrPlatformNames and arrConcernLevels> ADD CODE HERE
+    string arrPlatformNames[MAX_PLATFORMS];
+    int arrConcernLevels[MAX_PLATFORMS];
+
+    // ADD CODE HERE <Initialize both arrays with default values, at least 4 records> ADD CODE HERE
+    arrPlatformNames[0] = "Facebook";
+    arrConcernLevels[0] = 6;
+    arrPlatformNames[1] = "Google";
+    arrConcernLevels[1] = 7;
+    arrPlatformNames[2] = "Apple";
+    arrConcernLevels[2] = 9;
+    arrPlatformNames[3] = "WhatsApp";
+    arrConcernLevels[3] = 8;
+
+
+    do
+    {
+        cout << "\nMenu:\n";
+        cout << "1. View all platforms\n";
+        cout << "2. Add a new platform\n";
+        cout << "3. Update a platform's concern level\n";
+        cout << "4. Exit\n";
+        cout << "\nYour choice: ";
+        cin >> choice;
+
+        switch (choice)
+        {
+            case 1:
+                displayPlatforms(arrPlatformNames, arrConcernLevels, currentSize);
+                break;
+            case 2:
+                addPlatform(arrPlatformNames, arrConcernLevels, currentSize);
+                break;
+            case 3:
+                updateConcernLevel(arrPlatformNames, arrConcernLevels, currentSize);
+                break;
+            case 4:
+                cout << "Exiting...\n";
+                break;
+            default:
+                cout << "Invalid choice. Try again.\n";
+        }
+    }
+    while (choice != 4);
+
+    return 0;
+}
+
+
+// Function to add a new platform and its associated concern level
+void addPlatform(string arrPlatformNames[], int arrConcernLevels[], int &currentSize)
+{
+    if (currentSize < MAX_PLATFORMS)
+        {
+        string newPlatformName;
+        int newConcernLevel;
+
+        cout << "Enter platform name: ";
+        cin.ignore();
+        getline(cin, newPlatformName);
+
+        cout << "Enter concern level (1-10): ";
+        cin >> newConcernLevel;
+
+        arrPlatformNames[currentSize] = newPlatformName;
+        arrConcernLevels[currentSize] = newConcernLevel;
+
+        currentSize++;
+
+        cout << "Platform added successfully!\n";
+    }
+    else
+    {
+        cout << "Maximum number of platforms reached.\n";
+    }
+}
+
+
+// Function to update the concern level for an existing platform
+void updateConcernLevel(string arrPlatformNames[], int arrConcernLevels[], int currentSize)
+{
+    int platformNumber;
+
+    cout << "Enter the number of the platform you want to update: ";
+    cin >> platformNumber;
+
+    if (platformNumber >= 1 && platformNumber <= currentSize)
+    {
+        int newConcernLevel;
+
+        cout << "Enter the updated concern level (1-10): ";
+        cin >> newConcernLevel;
+
+        arrConcernLevels[platformNumber - 1] = newConcernLevel;
+
+        cout << "Concern level updated successfully!\n";
+    }
+    else
+    {
+        cout << "Invalid platform number.\n";
+    }
+}
